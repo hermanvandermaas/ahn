7eee5601ee88f17ab5879ba7cf63472249093a75
Herman van der Maas <herman@waywayway.nl>
2018-02-26 06:52:02 +0100

    ahn laag toevoegen

--- app/src/main/java/nl/waywayway/ahn/MainActivity.java
+++ app/src/main/java/nl/waywayway/ahn/MainActivity.java
@@ -18,6 +18,7 @@ implements OnMapReadyCallback
 {
 	private boolean dialogWasShowed = false;
 	private Context context;
+	private Bundle savedInstanceStateGlobal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,6 +27,7 @@ implements OnMapReadyCallback
         setContentView(R.layout.main);
 
 		context = this;
+		savedInstanceStateGlobal = savedInstanceState;
 
 		MapFragment mapFragment = (MapFragment) getFragmentManager()
             .findFragmentById(R.id.map);
@@ -45,13 +47,52 @@ implements OnMapReadyCallback
 	}
 
 	@Override
-	public void onMapReady(GoogleMap googleMap)
+	public void onMapReady(final GoogleMap googleMap)
 	{
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-							.title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+		// Zoom in op Nederland bij eerste opstart app
+		if (savedInstanceStateGlobal == null)
+		{
+			final LatLngBounds nederland = new LatLngBounds(new LatLng(50.75, 3.2), new LatLng(53.7, 7.22));
+			findViewById(R.id.map).post(new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(nederland, 1));
+					}
+				});
+		}
+		
+		TileOverlay ahn2 = googleMap.addTileOverlay(new TileOverlayOptions()
+													  .tileProvider(tileProvider));
     }
+	
+	TileProvider tileProvider = new UrlTileProvider(256, 256) {
+		@Override
+		public synchronized URL getTileUrl(int x, int y, int zoom) {
+			// The moon tile coordinate system is reversed.  This is not normal.
+			int reversedY = (1 << zoom) - y - 1;
+			String s = String.format(Locale.US, MOON_MAP_URL_FORMAT, zoom, x, reversedY);
+			URL url = null;
+			try {
+				url = new URL(s);
+			} catch (MalformedURLException e) {
+				throw new AssertionError(e);
+			}
+			return url;
+		}
+	};
+	
+	private boolean checkTileExists(int x, int y, int zoom) {
+		int minZoom = 12;
+		int maxZoom = 16;
+
+		if ((zoom < minZoom || zoom > maxZoom)) {
+			return false;
+		}
+
+		return true;
+	}
 
 	// Check beschikbaarheid Play Services
 	protected void isPlayServicesAvailable()
@@ -79,11 +120,13 @@ implements OnMapReadyCallback
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 	}
-	
-	private int getStatusBarHeight() {
+
+	private int getStatusBarHeight()
+	{
 		int result = 0;
 		int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-		if (resourceId > 0) {
+		if (resourceId > 0)
+		{
 			result = getResources().getDimensionPixelSize(resourceId);
 		}
 		return result;
