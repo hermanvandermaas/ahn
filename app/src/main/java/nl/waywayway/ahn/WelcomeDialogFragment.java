2e9b073d76bef6827be2716aba7be87d5d798b79
Herman van der Maas <herman@waywayway.nl>
2018-05-07 07:18:00 +0200

    Oude versie maps utils

--- app/src/main/java/nl/waywayway/ahn/WelcomeDialogFragment.java
+++ app/src/main/java/nl/waywayway/ahn/WelcomeDialogFragment.java
@@ -27,6 +27,7 @@ public class WelcomeDialogFragment extends DialogFragment
         // title by default, but your custom layout might not need it. So here you can
         // remove the dialog title, but you must call the superclass to get the Dialog.
         Dialog dialog = super.onCreateDialog(savedInstanceState);
+		dialog.setTitle(R.string.welcome_dialog_title);
         //dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         return dialog;
     }
